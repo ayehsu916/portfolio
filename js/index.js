3be89bdc30e2dafa538973c86d3b59e5a9e055d3
ayehsu916 <hsumyat7023@gmail.com>
2025-12-19 10:36:55 +0630

    core: button feature

--- js/index.js
+++ js/index.js
@@ -4,25 +4,23 @@ const mobileNavToggleLine2=document.querySelector(".mobile-nav-toggle-line2");
 const mobileNavToggleLine1=document.querySelector(".mobile-nav-toggle-line1");
 const mobileNavToggleLine3=document.querySelector(".mobile-nav-toggle-line3");
 
-mobileNavToggleBtn.addEventListener("click",()=>{
+function toggleMobileNav(){
     //animation for mobile nav links
-    mobileNavLinks.classList.toggle("active-mobile-nav-links");
+     mobileNavLinks.classList.toggle("active-mobile-nav-links");
 
     //animation for burger button
     mobileNavToggleLine2.classList.toggle("active-mobile-nav-toggle-line2");
     mobileNavToggleLine1.classList.toggle("active-mobile-nav-toggle-line1");
     mobileNavToggleLine3.classList.toggle("active-mobile-nav-toggle-line3");
+}
+
+mobileNavToggleBtn.addEventListener("click",()=>{
+    toggleMobileNav();
 });
 
 mobileNavLinks.addEventListener("click",(e)=>{
-    if((e.target.tagName="a")){
-    //animation for mobile nav links
-    mobileNavLinks.classList.toggle("active-mobile-nav-links");
-
-    //animation for burger button
-    mobileNavToggleLine2.classList.toggle("active-mobile-nav-toggle-line2");
-    mobileNavToggleLine1.classList.toggle("active-mobile-nav-toggle-line1");
-    mobileNavToggleLine3.classList.toggle("active-mobile-nav-toggle-line3");
+    if((e.target.tagName==="A")){
+        toggleMobileNav();
     }
 });
 
